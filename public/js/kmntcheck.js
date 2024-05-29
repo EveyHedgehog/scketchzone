bd4b2e8acbba3fb9c1f963b8f1a7e1da94b62315
Evil Sonic <Hyper_Evil_Sonic@msn.com>
2024-05-29 05:12:00 -0500

    Added a Commento++ status checker on the front page

---
+++ public/js/kmntcheck.js
@@ -0,0 +1,39 @@
+function pingURL() { 
+
+    // The custom URL entered by user 
+    var settings = { 
+
+        // Defines the configurations 
+        // for the request 
+        cache: false, 
+        dataType: "jsonp", 
+        async: true, 
+        crossDomain: true, 
+        url: "https://kmnt.scketchzone.com", 
+        method: "GET", 
+        timeout: 5000,
+        headers: { 
+        accept: "application/json", 
+        "Access-Control-Allow-Origin": "*", 
+        }, 
+
+        // Defines the response to be made 
+        // for certain status codes 
+        statusCode: { 
+        200: function (response) { 
+            document.getElementById("status").innerHTML = "You can leave a comment, they're on!"; 
+        }, 
+        400: function (response) { 
+            document.getElementById("status").innerHTML = "You can't leave a comment, they're off!";
+        }, 
+        0: function (response) { 
+            document.getElementById("status").innerHTML = "You can't leave a comment, they're off!";
+        }, 
+        }, 
+    }; 
+        // Sends the request and observes the response
+         $.ajax(settings).done(function (response) {
+            console.log(response);
+         });        
+
+}
